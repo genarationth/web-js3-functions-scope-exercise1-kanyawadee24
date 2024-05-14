27cc68d853408b624cb5f8451e3badd452884d9e
Kanyawadee kongkaew <kanyawadeekongkaew@partykywdd.local>
2024-05-14 14:18:37 +0700

    JS3_EX1

---
+++ function.js
@@ -0,0 +1,29 @@
+// Exercise 1.1 Create function following below
+function addNumbers(a, b) {
+  return a + b;
+}
+console.log(addNumbers(20, 3));
+
+function subtractNumbers(a, b) {
+  return a - b;
+}
+console.log(subtractNumbers(20, 3));
+
+function multiplyNumbers(a, b) {
+  return a * b;
+}
+console.log(multiplyNumbers(20, 3));
+
+function divideNumbers(a, b) {
+  return a % b;
+}
+console.log(divideNumbers(20, 3));
+
+// Exercise 1.2 Create Function Inform---
+function Inform(firstName, location, hobby) {
+  return (
+    "My name " + firstName + "I live in" + location + "and i enjoy w." + hobby
+  );
+}
+
+console.log(Inform("kanyawadee", "bangkok", "eating"));
